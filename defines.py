f2ce1384fff22b92d1bf3e529b1bf26ffc19d121
jekahamster <jekahamster@gmail.com>
2022-12-14 20:51:59 +0200

    Add new global vars

--- defines.py
+++ defines.py
@@ -2,4 +2,9 @@ import os
 import pathlib
 
 ROOT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+
 DOWNLOAD_PATH = ROOT_DIR / "downloads"
+COOKIES_PATH = ROOT_DIR / "cookies"
+WEBDRIVER_PATH = ROOT_DIR / "webdrivers" / "chromedriver_106.exe"
+DEFAULT_SESSION_FNAME = "last-saved.pkl"
+TIMEOUT = 300
