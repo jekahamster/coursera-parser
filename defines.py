f3fbbc73b0084a7c5d085190188051f807e7803d
jekahamster <jekahamster@gmail.com>
2023-03-03 01:28:41 +0200

    Add DEBUG constant

--- defines.py
+++ defines.py
@@ -3,6 +3,8 @@ import pathlib
 
 ROOT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 
+DEBUG = True
+
 DOWNLOAD_PATH = ROOT_DIR / "downloads"
 COOKIES_PATH = ROOT_DIR / "cookies"
 WEBDRIVER_PATH = ROOT_DIR / "webdrivers" / "chromedriver_106.exe"
