5bc91b74f2c05452d98fc6ac012ee04251570718
jekahamster <jekahamster@gmail.com>
2024-02-23 00:25:41 +0200

    Add DIRNAME_CHAR_COUNT constant

--- defines.py
+++ defines.py
@@ -8,7 +8,9 @@ ALLOW_LESSON_MISSING = False
 
 DOWNLOAD_PATH = ROOT_DIR / "downloads"
 COOKIES_PATH = ROOT_DIR / "cookies"
-WEBDRIVER_PATH = ROOT_DIR / "webdrivers" / "chromedriver_106.exe"
+WEBDRIVER_PATH = ROOT_DIR / "webdrivers" / "chromedriver.exe"
 EXTENSIONS_PATH = ROOT_DIR / "extensions"
 DEFAULT_SESSION_FNAME = "last-saved.pkl"
 TIMEOUT = 120
+# TIMEOUT = 30
+DIRNAME_CHAR_COUNT = 100
